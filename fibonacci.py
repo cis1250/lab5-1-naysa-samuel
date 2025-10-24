d1f352befb5e54d2adb23de21756a4b6a1c4c270
Naysa Samuel <naysa@uoguelph.ca>
2025-10-23 21:35:32 -0400

    Some work done on Fibonacci Sequence (NOT DONE)
    I am going to revise loops and functions as I am thinking of C rather than python right now.

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+def get_num_terms():
+  while True:
+    try:
+      n = int(input("How many terms of the Fibonacci sequence do you want? "))
+      if n>0:
+        return n
+      else:
+        print("Error: Please enter a positive integer.")
+    except ValueError:
+      print("Error: Invalid input. Please enter a positive integer. ")
 
+def generate_fibonacci(n):
+  sequence = []
+  a,b= 0,1
+  for _ in range(n):
+    #this....is a bit confusing. I'm going to watch a tutorial on append and how for loops work.
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
